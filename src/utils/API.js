d9997fd58ad97c88bb24d56babb576ff879610b2
cpusillo <cpusillo@gmail.com>
2021-01-22 18:11:34 -0500

    Fixed api typo

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getUser: function(){
-        return axios.get('https://randomuser.me/api/?results=120&nat=us');
+        return axios.get('https://randomuser.me/api/?results=150&nat=us');
     }
 }
